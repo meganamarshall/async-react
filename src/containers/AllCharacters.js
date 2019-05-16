74147d238af3dfa8a199208aefffe26a0116bcb8
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-16 16:10:08 -0700

    started allCharacters

---
+++ src/containers/AllCharacters.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Characters from '../components/Characters';
+
+export default class AllCharacters extends PureComponent {
+  state = {
+    characters: []
+  }
+
+  fetchCharacters = () => {
+    
+  }
+}
