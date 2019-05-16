058ab7221b26ffe48a78b9cdad583aa00b787530
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-16 13:44:55 -0700

    working on adding count for fetch to work

--- src/components/TopQuotes.js
+++ src/components/TopQuotes.js
@@ -1,8 +1,17 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import Quotes from './Quotes';
 import { getQuotes } from '../services/getQuote';
 
 export default class TopQuotes extends PureComponent {
+  static propTypes = {
+    count: PropTypes.number
+  }
+
+  static defaultProps = {
+    count: 3
+  }
+
   state = {
     quotes: []
   }
