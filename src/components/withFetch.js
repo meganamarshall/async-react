d1c486a01077d204e60dc04c7a5becd4a14796d8
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-17 15:51:30 -0700

    working on withFetch

---
+++ src/components/withFetch.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export const withFetch = (fetchFn, initialState, key = 'results') => Comp => {
+  class WithFetch {
+    static propTypes = {
+      page: PropTypes.number.isRequired
+    }
+  
+    state = {
+      results: initialState
+    }
+
+    fetch = () => {
+      this.setState({ loading: true });
+      fetchFn(this.props.page)
+        .then(results => this.setState({ results, loading: false }));
+    }
+
+    componentDidMount() {
+      this.fetch();
+    }
+
+    componentDidUpdate(prevProps) {
+      if(prevProps.page !== this.props.page) {
+        this.fetch();
+      }
+    }
+
+    render() {
+      const { results, loading } = this.state;
+      const props = { [key]: results, loading, ...this.props };
+      return <Comp {...props} />;
+    }
+  }
+  return WithFetch;
+};
