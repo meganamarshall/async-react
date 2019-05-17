fb2fdcf27873ec36c6a2da3b83a257acd73e3afc
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-17 11:46:05 -0700

    paging still not working

--- src/containers/AllCharacters.js
+++ src/containers/AllCharacters.js
@@ -12,9 +12,9 @@ export default class AllCharacters extends PureComponent {
     totalPages: 1
   }
 
-  fetchCharacters = () => {
+  fetchCharacters = currentPage => {
     this.setState({ loading: true });
-    getCharacters()
+    getCharacters(currentPage)
       .then(({ totalPages, characters }) => this.setState({ totalPages, characters, loading: false }));
   }
 
@@ -22,27 +22,29 @@ export default class AllCharacters extends PureComponent {
     this.fetchCharacters(this.currentPage);
   }
 
-  previousButton() {
-    if(this.state.currentPage === 1) throw 'this is the first page';
-    let thePage = this.state.currentPage;
-    this.setState({ currentPage: thePage-- });
+  previousButton(currentPage) {
+    if(currentPage === 1) throw 'this is the first page';
+    let thePage = currentPage;
+    console.log(thePage);
+    this.setState({ currentPage: thePage - 1 });
     this.fetchCharacters(this.currentPage - 1);
   }
 
-  nextButton() {
-    if(this.state.currentPage === this.state.totalPages) throw 'no more pages';
-    let thePage = this.state.currentPage;
-    this.setState({ currentPage: thePage++ });
+  nextButton(currentPage, totalPages) {
+    if(currentPage === totalPages) throw 'no more pages';
+    let thePage = currentPage;
+    console.log(thePage);
+    this.setState({ currentPage: thePage + 1 });
     this.fetchCharacters(this.currentPage + 1);
   }
 
   render() {
-    const { characters, loading } = this.state;
+    const { characters, loading, currentPage, totalPages } = this.state;
     if(loading) return <Loading />;
     return (
     <>
-      <button onClick={() => this.previousButton()}>Previous Page</button>
-      <button onClick={() => this.nextButton()}>Next Page</button>
+      <button onClick={() => this.previousButton(currentPage)}>Previous Page</button>
+      <button onClick={() => this.nextButton(currentPage, totalPages)}>Next Page</button>
       <Characters characters={characters} />
     </>
     );
