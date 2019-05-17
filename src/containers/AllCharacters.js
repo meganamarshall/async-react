acd3b759dd73bf4e309fa6784e240b61cfd32111
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-17 11:31:27 -0700

    working on paging

--- src/containers/AllCharacters.js
+++ src/containers/AllCharacters.js
@@ -7,22 +7,39 @@ import { getCharacters } from '../services/rickAndMortyApi';
 export default class AllCharacters extends PureComponent {
   state = {
     characters: [],
-    loading: true
+    loading: true,
+    currentPage: 1,
+    totalPages: 1
   }
 
   fetchCharacters = () => {
     this.setState({ loading: true });
     getCharacters()
-      .then(({ characters }) => this.setState({ characters, loading: false }));
+      .then(({ totalPages, characters }) => this.setState({ totalPages, characters, loading: false }));
   }
 
   componentDidMount() {
-    this.fetchCharacters();
+    this.fetchCharacters(this.currentPage);
+  }
+
+  'https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/'
+
+  previousButton() {
+    if(this.state.currentPage === this.state.totalPages) throw 'no more pages';
+    let thePage = this.state.currentPage;
+    this.setState({ currentPage: thePage-- });
+    this.fetchCharacters(this.currentPage - 1);
   }
 
   render() {
     const { characters, loading } = this.state;
     if(loading) return <Loading />;
-    return <Characters characters={characters} />;
+    return (
+    <>
+      <button onClick={() => previousButton()}>Previous Page</button>
+      <button>Next Page</button>
+      <Characters characters={characters} />
+    </>
+    );
   }
 }
--- src/services/rickAndMortyApi.js
+++ src/services/rickAndMortyApi.js
@@ -1,5 +1,6 @@
-export const getCharacters = () => {
-  return fetch('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/')
+export const getCharacters = currentPageNumber => {
+  if(!currentPageNumber) currentPageNumber = 1;
+  return fetch(`https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/?page=${currentPageNumber}`)
     .then(res => ([res.ok, res.json()]))
     .then(([ok, json]) => {
       if(!ok) throw 'Unable to fetch';
